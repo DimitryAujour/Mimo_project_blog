974723a933154206f40dbea0f891482c6568f502
DimitryAujour <106028495+DimitryAujour@users.noreply.github.com>
2023-09-05 09:38:51 -0400

    Many changes to the overall design,the nav bar has been added and the styles has beeen updated

--- script.js
+++ script.js
@@ -5,11 +5,33 @@ function clearBorders() {
     }
 }
 
-function setMainImage(id) {
-    const mainImage = document.querySelector("#mainImage");
-    const thumbnail = document.querySelector("#thumbnail" + id);
-    const thumbnailSrc = thumbnail.getAttribute("src");
-    mainImage.setAttribute("src", thumbnailSrc);
-    clearBorders();
-    thumbnail.classList.add("border");
+function setMainImage(imageNumber) {
+    const mainImage = document.getElementById("mainImage");
+    const descriptionBox = document.getElementById("mainDescription");
+
+    switch (imageNumber) {
+        case 1:
+            mainImage.src = "https://mimo.app/i/kittles.png";
+            descriptionBox.textContent = "Kittles: A delightful picture of a playful kitten.";
+            break;
+        case 2:
+            mainImage.src = "https://mimo.app/i/babaganoosh.png";
+            descriptionBox.textContent = "Babaganoosh: A mysterious image with a blend of colors.";
+            break;
+        case 3:
+            mainImage.src = "https://mimo.app/i/underwater.png";
+            descriptionBox.textContent = "Underwater: Dive into the deep blue and discover the secrets of the ocean.";
+            break;
+        case 4:
+            mainImage.src = "https://mimo.app/i/lighthouse.png";
+            descriptionBox.textContent = "Lighthouse: Standing tall, guiding ships through the night.";
+            break;
+        case 5:
+            mainImage.src = "https://mimo.app/i/market.png";
+            descriptionBox.textContent = "Market: A bustling marketplace filled with colors and energy.";
+            break;
+        default:
+            console.error("Unexpected image number:", imageNumber);
+            break;
+    }
 }
